1952c38be6cfa271ffdf4263b8e1abb56f368b5a
aman adinlid <95pinkpanda@gmail.com>
2020-12-14 23:46:53 +0100

    car practice

---
+++ src/main/java/org/example/car.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class car {
+
+    public static void main( String[] args ){
+
+        String carbrand = ("Volvo");
+        String ownername= ("Sarah");
+        System.out.println("ownername");
+        int regerstationnumber = 123;
+        System.out.println(regerstationnumber);
+        int maxspeed = 200;
+        System.out.println(maxspeed);
+        System.out.println("carbrand + ownername + regerstationnumber + maxspeed");
+
+    }
+
+}
