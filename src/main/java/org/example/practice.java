cb318c101950f66b8a72e8d0bbdffcd70a71af61
aman adinlid <95pinkpanda@gmail.com>
2020-12-14 23:40:35 +0100

    Initial commit

---
+++ src/main/java/org/example/practice.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class practice {
+
+
+    public static void main(String[] args) {
+        String firstname = "Aman";
+        String lastName = "Adinlid";
+        System.out.println(firstname + " " + lastName);
+
+        double d = 2.3;
+        double c = 1.9;
+        double result = 2.3 + 1.9;
+        System.out.println(result);
+    }
+}
